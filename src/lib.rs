1d660fde9735126f0a19df881a87a56078740543
hhs629629 <hhs629629@gmail.com>
2022-02-14 21:01:05 +0900

    Add IntoUtf8

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
+mod http_elements;
 mod request;
 mod response;
 
 mod basic_combinator;
 mod error;
-mod http_combinator;
-
-use nom::error::Error;
-use nom::Err;
 
 pub trait FromUtf8 {
-    fn from_utf8<'a>(buf: &'a [u8]) -> Result<Self, Err<Error<&[u8]>>>
+    fn from_utf8<'a>(buf: &'a [u8]) -> Result<Self, FromUtf8Err>
     where
         Self: Sized;
 }
+
+pub trait IntoUtf8 {
+    fn into_utf8(self) -> Result<Vec<u8>, ()>;
+}
+
+pub mod http_combinator;
+pub use crate::error::FromUtf8Err;
