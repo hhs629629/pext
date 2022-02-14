9b3bb8f197c76ee012a845576ef46f32cdcfa785
hhs629629 <hhs629629@gmail.com>
2022-02-14 20:59:46 +0900

    Implement FromUtf8Err

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,53 @@
+use std::fmt::Display;
 
+use nom::error::Error;
+use nom::Err;
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    Method,
+    Uri,
+    Version,
+    Header,
+}
+
+#[derive(Debug)]
+pub struct FromUtf8Err {
+    input: String,
+    kind: ErrorKind,
+}
+
+impl std::error::Error for FromUtf8Err {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
+
+impl Display for FromUtf8Err {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Error from input: [\"{}\"] ErrorKind: [{:?}]",
+            self.input, self.kind
+        )
+    }
+}
+
+pub trait IntoFromUtf8Err {
+    fn into_parse_error(self, kind: ErrorKind) -> FromUtf8Err;
+}
+
+impl IntoFromUtf8Err for Err<Error<&[u8]>> {
+    fn into_parse_error(self, kind: ErrorKind) -> FromUtf8Err {
+        let error = match self {
+            Err::Incomplete(_) => unimplemented!(),
+            Err::Error(e) => e,
+            Err::Failure(e) => e,
+        };
+
+        FromUtf8Err {
+            input: String::from_utf8(error.input.to_vec()).unwrap(),
+            kind,
+        }
+    }
+}
