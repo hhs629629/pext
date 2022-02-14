2b5c76204350cfdda9005b95b387ab95adeefead
hhs629629 <hhs629629@gmail.com>
2022-02-14 21:00:44 +0900

    Make from_utf8 return FromUtf8Err

--- src/request.rs
+++ src/request.rs
@@ -1,20 +1,23 @@
 use http::{Request, Version};
 use nom::{bytes::complete::*, combinator::*, multi::*, sequence::*};
 
-use nom::error::Error;
-use nom::Err;
-
+use crate::error::*;
 use crate::http_combinator::*;
 use crate::FromUtf8;
 
 impl FromUtf8 for Request<Vec<u8>> {
-    fn from_utf8<'a>(buf: &'a [u8]) -> Result<Self, Err<Error<&[u8]>>>
+    fn from_utf8<'a>(buf: &'a [u8]) -> Result<Self, FromUtf8Err>
     where
         Self: Sized,
     {
-        let (rest, method) = terminated(method, tag(" "))(buf)?;
-        let (rest, uri) = terminated(is_not(" "), tag(" "))(rest)?;
-        let (rest, http_version) = terminated(http_version, tag("\r\n"))(rest)?;
+        let (rest, method) =
+            terminated(method, tag(" "))(buf).map_err(|e| e.into_parse_error(ErrorKind::Method))?;
+
+        let (rest, uri) = terminated(is_not(" "), tag(" "))(rest)
+            .map_err(|e| e.into_parse_error(ErrorKind::Uri))?;
+
+        let (rest, http_version) = terminated(http_version, tag("\r\n"))(rest)
+            .map_err(|e| e.into_parse_error(ErrorKind::Version))?;
 
         let (rest, headers) = terminated(
             separated_list0(
@@ -25,12 +28,13 @@ impl FromUtf8 for Request<Vec<u8>> {
                 ),
             ),
             tag("\r\n\r\n"),
-        )(rest)?;
+        )(rest)
+        .map_err(|e| e.into_parse_error(ErrorKind::Header))?;
 
         let mut builder = Request::builder()
             .method(method)
             .uri(uri)
-            .version(guess_version(http_version));
+            .version(Version::from_utf8(http_version)?);
 
         for (name, value) in headers {
             builder = builder.header(name, value);
@@ -40,16 +44,6 @@ impl FromUtf8 for Request<Vec<u8>> {
     }
 }
 
-fn guess_version(version: &[u8]) -> Version {
-    match version {
-        b"HTTP/0.9" => Version::HTTP_09,
-        b"HTTP/1.0" => Version::HTTP_10,
-        b"HTTP/1.1" => Version::HTTP_11,
-        b"HTTP/2.0" => Version::HTTP_2,
-        b"HTTP/3.0" => Version::HTTP_3,
-        _ => unreachable!(),
-    }
-}
 
 #[cfg(test)]
 mod test {
@@ -59,14 +53,17 @@ mod test {
 
     #[test]
     fn test_from_utf8() {
-        let something = b"GET /hello.htm HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\nHost: www.tutorialspoint.com\r\nAccept-Language:\r\nAccept-Encoding: gzip, deflate\r\nConnection: Keep-Alive\r\n\r\nThisIsBody";
+        let input = b"GET /hello.htm HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\nHost: www.tutorialspoint.com\r\nAccept-Language:\r\nAccept-Encoding: gzip, deflate\r\nConnection: Keep-Alive\r\n\r\nThisIsBody";
 
-        let req = Request::from_utf8(something).unwrap();
+        let req = Request::from_utf8(input).unwrap();
 
         assert_eq!(req.method(), Method::GET);
         assert_eq!(req.uri(), "/hello.htm");
         assert_eq!(req.version(), Version::HTTP_11);
-        assert_eq!(req.headers().get("user-agent").unwrap(), "Mozilla/4.0 (compatible; MSIE5.01; Windows NT)");
+        assert_eq!(
+            req.headers().get("user-agent").unwrap(),
+            "Mozilla/4.0 (compatible; MSIE5.01; Windows NT)"
+        );
         assert_eq!(req.headers().get("Host").unwrap(), "www.tutorialspoint.com");
         assert_eq!(req.headers().get("Accept-Language").unwrap(), "");
         assert_eq!(req.body(), b"ThisIsBody");
