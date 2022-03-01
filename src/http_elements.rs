22ad8472a3a695965e3f306fd8e465558140b7ac
hhs629629 <hhs629629@gmail.com>
2022-03-01 16:48:29 +0900

    Implement IntoUtf8 for http elements

--- src/http_elements.rs
+++ src/http_elements.rs
@@ -1,18 +1,50 @@
-use http::Version;
+use http::{HeaderMap, StatusCode, Version};
 
-use crate::*;
+use crate::{FromUtf8, FromUtf8Err, IntoUtf8};
 
 impl FromUtf8 for Version {
     fn from_utf8<'a>(buf: &'a [u8]) -> Result<Self, FromUtf8Err>
     where
-        Self: Sized {
-            Ok(match buf {
-                b"HTTP/0.9" => Version::HTTP_09,
-                b"HTTP/1.0" => Version::HTTP_10,
-                b"HTTP/1.1" => Version::HTTP_11,
-                b"HTTP/2.0" => Version::HTTP_2,
-                b"HTTP/3.0" => Version::HTTP_3,
-                _ => unreachable!(),
-            })
+        Self: Sized,
+    {
+        Ok(match buf {
+            b"HTTP/0.9" => Version::HTTP_09,
+            b"HTTP/1.0" => Version::HTTP_10,
+            b"HTTP/1.1" => Version::HTTP_11,
+            _ => unreachable!(),
+        })
+    }
+}
+
+impl IntoUtf8 for Version {
+    fn into_utf8(&self) -> Result<Vec<u8>, ()> {
+        Ok(match self {
+            &Version::HTTP_09 => b"HTTP/0.9",
+            &Version::HTTP_10 => b"HTTP/1.0",
+            &Version::HTTP_11 => b"HTTP/1.1",
+            _ => unreachable!(),
+        }
+        .to_vec())
+    }
+}
+
+impl IntoUtf8 for StatusCode {
+    fn into_utf8(&self) -> Result<Vec<u8>, ()> {
+        Ok(self.to_string().as_bytes().to_vec())
+    }
+}
+
+impl IntoUtf8 for HeaderMap {
+    fn into_utf8(&self) -> Result<Vec<u8>, ()> {
+        let mut result = Vec::new();
+
+        for (key, value) in self.iter() {
+            result.append(&mut key.as_str().as_bytes().to_vec());
+            result.append(&mut b": ".to_vec());
+            result.append(&mut value.as_bytes().to_vec());
+            result.append(&mut b"\r\n".to_vec());
+        }
+
+        Ok(result)
     }
 }
